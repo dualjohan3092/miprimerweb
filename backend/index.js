543723bde7be662638ff609aa3dcf33afddf15e8
johanbohorquez2021 <johan.bohorquez@perficient.com>
2024-04-19 17:50:08 -0500

    First changes for this project

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,28 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.BACKEND_LOCAL_PORT || 3000; // Utiliza el puerto definido en la variable de entorno o 3000 por defecto
+const backendIP = process.env.BACKEND_IP || 'localhost'; // Utiliza la dirección IP definida en la variable de entorno o 'localhost' por defecto
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  let fechaHoraActual = new Date();
+  let textoFechaHora = fechaHoraActual.toLocaleString();
+  let msg = `¡Hola Mundo desde el backend! ${textoFechaHora}`;
+  console.log(textoFechaHora);
+  res.send(msg);
+});
+
+app.listen(port, backendIP, () => {
+  console.log(`El servidor del backend está corriendo en http://${backendIP}:${port}`);
+});
+
+/*
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
 const port = 3000;
 app.use(cors());
 
@@ -16,3 +38,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`El servidor del backend está corriendo en http://localhost:${port}`);
 });
+*/
--- front-angular/src/app/home/home-service.service.ts
+++ front-angular/src/app/home/home-service.service.ts
@@ -7,6 +7,30 @@ import { Observable } from 'rxjs';
 })
 export class HomeServiceService {
 
+  backendLocalPort: string = process.env.BACKEND_LOCAL_PORT || '3000'; // Puerto local del backend
+  backendIP: string = process.env.BACKEND_IP || 'localhost'; // Dirección IP del backend
+  apiUrl: string = `http://${this.backendIP}:${this.backendLocalPort}/`; // URL de tu API
+
+  constructor(private http: HttpClient) { }
+
+  obtenerDatos(): Observable<any> {
+    return this.http.get(this.apiUrl, { responseType: 'text' });
+  }
+
+}
+
+
+
+/*
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeServiceService {
+
   apiUrl: string = 'http://localhost:3000/'; // URL de tu API
 
   constructor(private http: HttpClient) { }
@@ -16,3 +40,4 @@ export class HomeServiceService {
   }
 
 }
+*/
